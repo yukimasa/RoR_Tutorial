42f0a146880c46c58fb94bac75c0aa27fafb5ce8
Yukimasa Ikeda <ykms123456789@gmail.com>
2019-06-15 19:02:55 +0900

    Done until 7.3.3

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
-class UserControllerTest < ActionDispatch::IntegrationTest
+class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get signup_path
     assert_response :success
   end
-
 end
